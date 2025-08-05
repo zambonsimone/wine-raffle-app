4a119a302025b28e3c14835b3a22c449c7321a6d
Simone Zambon <simone.zambon@grupposandonato.it>
2025-08-05 22:42:00 +0200

    add color definition list

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
+import { DefinitionList } from './component/DefinitionList'
 import { TicketReservationPanel } from './component/TicketReservationPanel'
 
 export const App: React.FC = () => {
     return (
-        <main className="w-fit px-[2rem] mx-auto my-[2rem] bg-gray-200 shadow-md rounded">
+        <main className="w-fit px-[2rem] mx-auto md:my-[2rem] bg-gray-200 shadow-md rounded">
             <h1 className="text-3xl font-bold mb-4 text-center pt-[2rem] pb-[0.5rem]">Wine Raffle</h1>
-            <h2 className="text-1xl font-medium mb-4 text-center pt-[0.2rem] pb-[2rem]">Purchase at least one ticket to enter the lottery</h2>
+            <h2 className="text-2xl font-medium mb-4 text-center pt-[0.2rem] pb-[2rem]">Purchase at least one ticket to enter the lottery</h2>
+            <DefinitionList />
             <TicketReservationPanel />
         </main>
     )
---
+++ src/component/DefinitionList.tsx
@@ -0,0 +1,29 @@
+import type React from "react"
+
+const ColorSample: React.FC<{ color: string }> = ({
+    color
+}) => {
+    return (
+        <div className={`rounded bg-${color}-400 w-[1rem] h-[1rem]`}></div>
+    )
+}
+
+export const DefinitionList: React.FC = () => {
+    const itemClassName = "flex text-1xl font-normal gap-[1rem] items-center uppercase"
+    return (
+        <dl className="my-[2rem]">
+            <div role="presentation" className={itemClassName}>
+                <dt><ColorSample color="red"/></dt>
+                <dd>NOT Available</dd>
+            </div>
+            <div role="presentation" className={itemClassName}>
+                <dt><ColorSample color="green"/></dt>
+                <dd>Available</dd>
+            </div>
+            <div role="presentation" className={itemClassName}>
+                <dt><ColorSample color="blue"/></dt>
+                <dd>Selected</dd>
+            </div>
+        </dl>
+    )
+}
