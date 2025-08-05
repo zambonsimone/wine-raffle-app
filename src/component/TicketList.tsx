ddd7a1a2f19b5d1e395f35adc41695c806759d26
Simone Zambon <simone.zambon@grupposandonato.it>
2025-08-06 00:03:44 +0200

    fix ticket list grid

--- src/component/TicketList.tsx
+++ src/component/TicketList.tsx
@@ -8,12 +8,6 @@ interface ITicketListProps {
     loading: boolean;
 }
 
-const gridMobile = "grid-cols-[repeat(4,_minmax(0,_6rem))]";
-const gridTablet = "grid-cols-[repeat(5,_minmax(0,_6rem))]";
-const gridDesktop = "grid-cols-[repeat(6,_minmax(0,_6rem))]";
-const gridFull = "grid-cols-[repeat(8,_minmax(0,_6rem))]";
-const gridResponsiveTemplate = `grid ${gridMobile} lg:${gridFull} md:${gridDesktop} sm:${gridTablet} gap-[2vw]`;
-
 export const TicketList: React.FC<ITicketListProps> = ({
     currentSelection,
     onSelectTicket,
@@ -22,7 +16,7 @@ export const TicketList: React.FC<ITicketListProps> = ({
     const { tickets } = useTicketsStore();
 
     return (
-        <div className={`${gridResponsiveTemplate} ${loading ? "opacity-30" : ""}`}>
+        <div className={`grid grid-cols-[repeat(4,_minmax(0,_6rem))] lg:grid-cols-[repeat(8,_minmax(0,_6rem))] md:grid-cols-[repeat(6,_minmax(0,_6rem))] sm:grid-cols-[repeat(5,_minmax(0,_6rem))] gap-[2vw] ${loading ? "opacity-30" : ""}`}>
             { tickets.map(ticket => (
                 <Ticket 
                     ticket={ticket} 
